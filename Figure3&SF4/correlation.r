4b4e5c001c6ef9f2bb0ba9be6ba21bc0bc6ba586
JiaxinWang6542 <wangjiaxin@sjtu.edu.cn>
2025-09-29 22:25:09 +0800

    Add files via upload

---
+++ Figure3&SF4/correlation.r
@@ -0,0 +1,50 @@
+
+setwd("/work/wjx/scRNA-seq/AIP/figure/figure3")
+library(Seurat)
+library(ggplot2)
+library(pheatmap)
+library(tidyverse)
+library(ggsci)
+library(magrittr)
+library(ComplexHeatmap)
+
+WholeTissueData_B <- readRDS("/work/wjx/scRNA-seq/AIP/figure/figure2/WholeTissueData_B_ident.rds.gz")
+WholeTissueData_myeloid <- readRDS("/work/wjx/scRNA-seq/AIP/figure/figure5/WholeTissueData_macrophage_ident.rds.gz")
+
+a <- list(WholeTissueData_B,WholeTissueData_myeloid)
+
+names(a) <- c("Bcell", "Myeloid")
+
+freq <- lapply(a,function(x){
+  EachSampleClusterDisB <-lapply(unique(x@meta.data$orig.ident),function(i){
+    seur <- subset(x,cells=rownames(x@meta.data[x@meta.data$orig.ident %in% i,]))
+    seur@meta.data$celltype1 %>% table %>% data.frame %>% set_colnames(c("CellTypes","Number")) %>% dplyr::mutate(Per=100*Number/sum(Number))
+  })
+  names(EachSampleClusterDisB) <- unique(x@meta.data$orig.ident)
+  EachSampleClusterDisC <- dplyr::bind_rows(EachSampleClusterDisB) %>% dplyr::mutate(Samples=rep(names(EachSampleClusterDisB),times=unlist(lapply(EachSampleClusterDisB,nrow))))
+  EachSampleClusterDisC$Tissue <- gsub("N.*","Normal",EachSampleClusterDisC$Samples)
+  EachSampleClusterDisC$Tissue <- gsub("P.*","AIP",EachSampleClusterDisC$Tissue)
+  return(EachSampleClusterDisC)})
+
+freq_data <-bind_rows(freq)
+freq_data <- freq_data[,c(1,3,4)]
+data <- spread(freq_data,key="CellTypes",value="Per",fill=0)
+rownames(data) <- data[,1]
+data <- data[,-1]
+data <- data[,c(4,8:12)]
+expr <- cor(data,method = "spearman")
+
+library(ComplexHeatmap)
+pdf("Figure3e.pdf",width = 5,height = 4.7)
+Heatmap(expr,
+        cluster_columns = T,clustering_distance_columns="euclidean",
+        cluster_rows = T,clustering_distance_rows="euclidean",
+        show_row_dend = F,column_dend_height = unit(20, "mm"),
+        row_names_side = 'left',
+        border = T,
+        rect_gp = gpar(col = NA),
+        show_column_names = T,
+        col=colorRampPalette(rev(RColorBrewer::brewer.pal(10,"RdYlBu")),space="rgb")(100),
+        heatmap_legend_param = list(title = "Correlation",title_gp = gpar(fontsize = 12, fontface = "bold"),legend_height=unit(50, "mm")),
+)
+dev.off()
